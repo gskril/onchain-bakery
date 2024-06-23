3269aab1e587fc9d9be038dc3883d4fd0a05a45d
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-06-23 04:28:27 -0400

    Start smart contracts

---
+++ contracts/src/ProofOfBread.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+
+///////////////////////////////////////////////////////////
+//                                                       //
+//       ____     ____    U _____ u    _      ____       //
+//    U | __")uU |  _"\ u \| ___"|/U  /"\  u |  _"\      //
+//     \|  _ \/ \| |_) |/  |  _|"   \/ _ \/ /| | | |     //
+//      | |_) |  |  _ <    | |___   / ___ \ U| |_| |\    //
+//      |____/   |_| \_\   |_____| /_/   \_\ |____/ u    //
+//     _|| \\_   //   \\_  <<   >>  \\    >>  |||_       //
+//    (__) (__) (__)  (__)(__) (__)(__)  (__)(__)_)      //
+//                                                       //
+///////////////////////////////////////////////////////////
+
+contract ProofOfBread is ERC1155, Ownable, ERC1155Supply {
+    /*//////////////////////////////////////////////////////////////
+                              CONSTRUCTOR                          
+    //////////////////////////////////////////////////////////////*/
+
+    constructor(
+        address _owner,
+        string memory _uri
+    ) ERC1155(_uri) Ownable(_owner) {}
+
+    /*//////////////////////////////////////////////////////////////
+                             PUBLIC ACTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function collectBread(
+        address account,
+        uint256 id,
+        bytes memory data
+    ) public onlyOwner {
+        // TODO: Verify the signature `data` to ensure it's signed by the backend
+        _mint(account, id, 1, data);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                          PERMISSIONED ACTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function setURI(string memory _uri) public onlyOwner {
+        _setURI(_uri);
+    }
+
+    function adminMint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public onlyOwner {
+        _mint(account, id, amount, data);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                           REQUIRED OVERRIDES
+    //////////////////////////////////////////////////////////////*/
+
+    function _update(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory values
+    ) internal override(ERC1155, ERC1155Supply) {
+        super._update(from, to, ids, values);
+    }
+}

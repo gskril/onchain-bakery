68b696c0f229ece6d54bbc287e43466d67b42b3b
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-07-01 03:27:21 -0400

    Fix deploy script

--- contracts/scripts/deploy-bread.ts
+++ contracts/scripts/deploy-bread.ts
@@ -3,6 +3,7 @@ import hre from 'hardhat'
 async function main() {
   const contract = await hre.viem.deployContract('Bread', [
     '0x179A862703a4adfb29896552DF9e307980D19285', // _owner,
+    '0x0000000000000000000000000000000000000000', // _signer,
     '0x0000000000000000000000000000000000000000', // _proofOfBread,
     'https://website.com/api/{id}', // _uri
   ])

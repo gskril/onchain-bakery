368fbc1fbfd110c412bceab8688c4a1c5ef1433e
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-07-02 17:41:20 -0400

    Update vanity script

--- contracts/scripts/vanity.ts
+++ contracts/scripts/vanity.ts
@@ -1,18 +1,17 @@
-import { Hex, getContractAddress } from 'viem'
+import hre from 'hardhat'
+import { Hex, getContractAddress, toHex } from 'viem'
 import { generatePrivateKey } from 'viem/accounts'
 import { privateKeyToAccount } from 'viem/accounts'
 
-const baseNonce = 69n
-// generateDeployerPrivateKey('0x0000', baseNonce)
-// generateNonce('0x0000', baseNonce)
+// generateDeployerPrivateKey('0x000')
+// generateCreate2Salt('0x000')
 
 /**
- * Generate a private key that will deploy a smart contract with a vanity address at a given nonce.
+ * Generate a private key that will deploy a smart contract with a vanity address.
  *
  * @param vanity The first few characters of the address you want to generate, case insensitive.
- * @param nonce Generally good to set this higher than 0 to prevent accidentally using the nonce on a different transaction.
  */
-function generateDeployerPrivateKey(vanity: Hex, nonce: bigint) {
+function generateDeployerPrivateKey(vanity: Hex) {
   let tries = 0
 
   while (true) {
@@ -22,7 +21,7 @@ function generateDeployerPrivateKey(vanity: Hex, nonce: bigint) {
 
     const deployedContractAddress = getContractAddress({
       from: deployerPublicAddress,
-      nonce,
+      nonce: 0n,
     })
 
     if (deployedContractAddress.toLowerCase().startsWith(vanity)) {
@@ -30,7 +29,6 @@ function generateDeployerPrivateKey(vanity: Hex, nonce: bigint) {
         deployerPrivateKey,
         deployerPublicAddress,
         deployedContractAddress,
-        nonce,
       })
 
       break
@@ -45,28 +43,31 @@ function generateDeployerPrivateKey(vanity: Hex, nonce: bigint) {
 }
 
 /**
- * Generate a nonce that will deploy a smart contract with a vanity address from the `process.env.DEPLOYER_KEY`.
+ * Generate a salt that will deploy a smart contract with a vanity address using CREATE2.
  *
  * @param vanity The first few characters of the address you want to generate, case insensitive.
- * @param baseNonce The nonce to start from.
  */
-function generateNonce(vanity: Hex, baseNonce: bigint) {
+async function generateCreate2Salt(vanity: Hex) {
   let tries = 0
-  let nonce = baseNonce + 1n
 
   const deployerPrivateKey = process.env.DEPLOYER_KEY as Hex
   const deployerPublicAddress = privateKeyToAccount(deployerPrivateKey).address
+  const artifacts = await hre.artifacts.readArtifact('Bread')
 
   while (true) {
+    const salt = toHex(tries)
+
     const deployedContractAddress = getContractAddress({
+      bytecode: artifacts.bytecode,
       from: deployerPublicAddress,
-      nonce,
+      opcode: 'CREATE2',
+      salt,
     })
 
     if (deployedContractAddress.toLowerCase().startsWith(vanity)) {
       console.log({
         deployedContractAddress,
-        nonce,
+        salt,
       })
 
       break
@@ -77,6 +78,5 @@ function generateNonce(vanity: Hex, baseNonce: bigint) {
     }
 
     tries++
-    nonce++
   }
 }

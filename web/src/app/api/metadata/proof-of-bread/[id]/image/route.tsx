50a584364e2657fb7f4cfde0d061b2f92b846799
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-07-26 03:31:33 -0400

    Add NFT metadata

---
+++ web/src/app/api/metadata/proof-of-bread/[id]/image/route.tsx
@@ -0,0 +1,25 @@
+import { ImageResponse } from 'next/og'
+import { NextRequest, NextResponse } from 'next/server'
+import { z } from 'zod'
+
+export const runtime = 'edge'
+
+const schema = z.object({
+  id: z.coerce.bigint(),
+})
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: [key: string] }
+): Promise<ImageResponse | NextResponse> {
+  const safeParse = schema.safeParse(params)
+
+  if (!safeParse.success) {
+    return NextResponse.json(safeParse.error, { status: 400 })
+  }
+
+  const { id } = safeParse.data
+  const DOMAIN = new URL(process.env.DOMAIN || 'http://localhost:3000').origin
+
+  return fetch(`${DOMAIN}/proof-of-bread/${id}.svg`)
+}
---
+++ web/src/app/api/metadata/proof-of-bread/[id]/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from 'next/server'
+import z from 'zod'
+
+const proofOfBreads = [
+  {
+    name: 'Proof of Bread #1',
+    description: 'This NFT is proof that you picked up bread from Greg in New York City on July 21, 2024.',
+  }
+]
+
+const schema = z.object({
+  id: z.coerce.bigint(),
+})
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: [key: string] }
+) {
+  const safeParse = schema.safeParse(params)
+
+  if (!safeParse.success) {
+    return NextResponse.json(safeParse.error, { status: 400 })
+  }
+
+  const { id } = safeParse.data
+  const nft = proofOfBreads[Number(id) - 1]
+
+  if (!nft) {
+    return NextResponse.json({ error: 'NFT not found' }, { status: 404 })
+  }
+
+  const DOMAIN = new URL(process.env.DOMAIN || 'http://localhost:3000').origin
+
+  const metadata = {
+    ...nft,
+    image: `${DOMAIN}/api/metadata/proof-of-bread/${id}/image`,
+  }
+
+  return NextResponse.json(metadata)
+}

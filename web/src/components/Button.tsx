5d7fc93c495723ea827dc2c0b80d50340409f9a3
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-08-09 16:37:34 -0400

    Add `<Spinner />`, improve `useCart()`

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 
 import { cn } from '@/lib/utils'
 
+import { Spinner } from './Spinner'
+
 export const buttonStyles = cva(
   'flex flex-row gap-2 items-center justify-center font-pangram border-brand-primary w-fit rounded-full border px-4 py-1 text-center text-xl uppercase disabled:cursor-not-allowed',
   {
@@ -34,29 +36,7 @@ export function Button({
       disabled={props.disabled || loading}
       {...props}
     >
-      {loading && (
-        <svg
-          className="text-brand-primary h-4 w-4 animate-spin"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          data-motion-id="svg 2"
-        >
-          <circle
-            className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="4"
-          ></circle>
-          <path
-            className="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          ></path>
-        </svg>
-      )}
+      {loading && <Spinner />}
 
       <span className="pt-[0.0625rem]">{children}</span>
     </button>
@@ -94,29 +74,7 @@ export function ButtonFilled({
       disabled={props.disabled || loading}
       {...props}
     >
-      {loading && (
-        <svg
-          className="text-brand-background-primary h-4 w-4 animate-spin"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          data-motion-id="svg 2"
-        >
-          <circle
-            className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="4"
-          ></circle>
-          <path
-            className="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          ></path>
-        </svg>
-      )}
+      {loading && <Spinner color="brand-background-primary" />}
 
       {children}
     </button>
--- web/src/hooks/useCart.ts
+++ web/src/hooks/useCart.ts
@@ -1,23 +1,31 @@
+import { useCallback, useMemo } from 'react'
 import { useLocalStorage } from 'usehooks-ts'
 
 export function useCart() {
   // useLocalStorage() doesn't like bigints, so we store them as numbers internally but export them as bigint for better type safety in other files
   const [_cart, setCart] = useLocalStorage<Array<number>>('cart', [])
 
-  const cart = _cart.map((i) => BigInt(i))
+  // Memoize the cart to maintain a stable reference
+  const cart = useMemo(() => _cart.map((i) => BigInt(i)), [_cart])
 
-  function addToCart(item: bigint) {
-    console.log(item, cart.includes(item))
+  const addToCart = useCallback(
+    (item: bigint) => {
+      console.log(item, cart.includes(item))
 
-    // avoid duplicates
-    if (!cart.includes(item)) {
-      setCart([..._cart, Number(item)])
-    }
-  }
+      // avoid duplicates
+      if (!cart.includes(item)) {
+        setCart((prevCart) => [...prevCart, Number(item)])
+      }
+    },
+    [cart, setCart]
+  )
 
-  function removeFromCart(item: bigint) {
-    setCart(_cart.filter((i) => i !== Number(item)))
-  }
+  const removeFromCart = useCallback(
+    (item: bigint) => {
+      setCart((prevCart) => prevCart.filter((i) => i !== Number(item)))
+    },
+    [setCart]
+  )
 
   return { cart, addToCart, removeFromCart }
 }
